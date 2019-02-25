4c0738fda77d869e5b862404cae77a53872633a5
wangchg <wangchg@hotmail.com>
2019-02-25 17:25:05 +0800

    init

---
+++ example-ws/src/main/java/com/wangchg/study/spring/example/ws/ExampleWsApplication.java
@@ -0,0 +1,13 @@
+package com.wangchg.study.spring.example.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleWsApplication.class, args);
+    }
+
+}
---
+++ example-ws/src/test/java/com/wangchg/study/spring/example/ws/ExampleWsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wangchg.study.spring.example.ws;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExampleWsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
